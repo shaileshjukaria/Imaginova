0d9cf32d91249efa0c3445d2f953ce52e53bae30
shaileshjukaria <sjukaria745@gmail.com>
2025-11-25 20:03:47 +0530

    Add AppContext for user state management and update Navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {assets} from '../assets/assets';
 import { Link, useNavigate } from 'react-router-dom';
+import { use } from 'react';
+import { AppContext } from '../context/AppContext';
 
 const Navbar = () => {
 
-    const [user, setUser] = React.useState(true);
+    const {user} = useContext(AppContext)
 
     const navigate = useNavigate();
   
@@ -24,16 +26,19 @@ const Navbar = () => {
           hover:scale-105 transition-all duration-700'>
             <img className='w-5' src={assets.credit_star} 
             alt=''/>
-            <p>Credts left : 50</p>
+            <p className='text-xs sm:text-sm font-medium
+            text-gray-600'> Credits left : 50</p>
           </button>
-          <p>Hi,Shailesh</p>
+          <p className='text-gray-600 max-sm:hidden pl-4'>Hi,Shailesh</p>
           <div className='relative group'>
             <img src={assets.profile_icon} className='w-10 drop-shadow'
             alt=''/>
             <div className='absolute hidden group-hover:block
               top-0 right-0 z-10 text-black rounded pt-12'>
-                <ul>
-                  <li>Logout</li>
+                <ul className='list-none m-0 p-2 bg-white
+                rounded-md border text-sm'>
+                  <li className='py-1 px-2 cursor-pointer
+                  pr-10'>Logout</li>
                 </ul>
           </div>
           </div>
