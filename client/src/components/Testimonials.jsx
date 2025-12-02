1464356fcafda511c2a6c93e345f29eb2088eb06
shaileshjukaria <sjukaria745@gmail.com>
2025-12-02 16:40:41 +0530

    upated testimonials

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
+import { testimonialsData } from '../assets/assets'   // make sure this is correct
 
 const Testimonials = () => {
   return (
-    <div className='flex flex-col items-center justify-center
-    my-20 py-12 '>
+    <div className='flex flex-col items-center justify-center my-20 py-12'>
       
-      <h1 className='text-3xl sm:text-4xl font-semibold
-      mb-2'>Customer Testimonials</h1>
+      <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>
+        Customer Testimonials
+      </h1>
+
       <p className='text-gray-500 mb-12'>What our users are Saying</p>
 
-        <div className='flex flex-wrap gap-6'>
+      <div className='flex flex-wrap gap-6'>
+        {testimonialsData.map((testimonial, index) => (
+          <div key={index}>
+            <div>
+              <img 
+                src={testimonial.image} alt="" 
+                className='rounded-full w-14'
+              />
+            </div>
+          </div>
+        ))}
+      </div>
 
-        </div>
     </div>
   )
 }
