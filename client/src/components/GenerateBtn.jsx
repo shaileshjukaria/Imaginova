d29ce3d00bc30189b16ab4cf1bf24e5f64dc03d5
shaileshjukaria <sjukaria745@gmail.com>
2025-12-03 20:53:16 +0530

    Add Footer component and update GenerateBtn styles for improved layout

--- client/src/components/GenerateBtn.jsx
+++ client/src/components/GenerateBtn.jsx
@@ -7,7 +7,7 @@ const GenerateBtn = () => {
       <h1 className='text-2xl md:text-3xl lg:text-4xl mt-4
       font-semibold text-neutral-800 py-6 md:py-16'>See the Magic. Try now</h1>
 
-      <button className='inline-flex items-center gap-2 px-2 px-12 py-3
+      <button className='inline-flex items-center gap-2 px-12 py-3
       rounded-full bg-black text-white m-auto hover:scale-105
       transition-all duration-500'>Generate Images
         <img src={assets.star_group} alt="" className='h-6' />
