53be8d6266d0cc0a824c5793bb47b1207724a611
shaileshjukaria <sjukaria745@gmail.com>
2025-12-03 22:42:28 +0530

    Refactor Result component to display an image with a transition effect

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { assets } from '../assets/assets';
 
 const Result = () => {
   return (
     <div>
-      Result
+      <div className='relative'>
+        <img src={assets.sample_img_1} alt='' className='max-w-sm rounded' />
+        <span className='absolute bottom-0 left-0 h-1 bg-blue-500
+        w-full transition-all duration-[10s]' />
+      </div>
     </div>
   );
 };
