cae6a62cae815382c2d457bfd47f8416182937d9
shaileshjukaria <sjukaria745@gmail.com>
2025-12-04 14:33:39 +0530

    Update pricing and credits in plans; enhance BuyCredit page layout with plan selection feature

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -83,20 +83,20 @@ export const testimonialsData = [
 export const plans = [
     {
       id: 'Basic',
-      price: 10,
+      price: 199,
       credits: 100,
       desc: 'Best for personal use.'
     },
     {
       id: 'Advanced',
-      price: 50,
-      credits: 500,
+      price: 599,
+      credits: 400,
       desc: 'Best for business use.'
     },
     {
       id: 'Business',
-      price: 250,
-      credits: 5000,
+      price: 2949,
+      credits: 2500,
       desc: 'Best for enterprise use.'
     },
   ]
