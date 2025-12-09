aa26c215ff82e3cf03e0fab541f26e662f3eba28
shaileshjukaria <sjukaria745@gmail.com>
2025-12-09 21:10:03 +0530

    Add API endpoint documentation for user registration and login routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -7,3 +7,6 @@ userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
 
 export default userRouter;
+
+// http://localhost:4000/api/user/register
+// http://localhost:4000/api/user/login
