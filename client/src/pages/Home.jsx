68ddd3710c59f4d71c8fb7f50eff00e15e933811
shaileshjukaria <sjukaria745@gmail.com>
2025-12-18 14:16:09 +0530

    fix: Remove unused ReviewSection import from Home component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,6 @@ import Header from '../components/Header';
 import Steps from '../components/Steps';
 import Description from '../components/Description';
 import Testimonials from '../components/Testimonials';
-import ReviewSection from '../components/ReviewSection';
 import GenerateBtn from '../components/GenerateBtn';
 
 const Home = () => {
@@ -13,7 +12,6 @@ const Home = () => {
       <Steps/>
       <Description/>
       <Testimonials/>
-      <ReviewSection/>
       <GenerateBtn/>
     </div>
   )
